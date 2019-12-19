8d814a9b29995986f42fc479f9f663e20ef65be0
Zanfir Bogdan-Andrei <56277980+Ragnarok54@users.noreply.github.com>
2019-12-20 01:08:20 +0200

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -1,40 +1,15 @@
 #include "node.h"
 
-void Node::addChild(Node* node) {
-	this->child.push_back(unique_ptr<Node> (node));
+void Node::addChild(Node& node) {
+	this->child.push_back(unique_ptr<Node>(&node));
 }
 
-bool Node::isChild(Node* node) {
-	list<unique_ptr<Node>> ::iterator it = child.begin(), a;
-	bool x = 0;
-	
-	for (it = child.begin(); it != child.end(); it++) {
-		if (it->get() == node)
-			x = 1;
-		x = it->get()->isChild(node);
-	}
-	return x;
-}
-
-Node* Node::getChild(int i) {
+Node& Node::getChild(int i) {
 	list<unique_ptr<Node>> ::iterator it = child.begin();
 
 	advance(it, i);
-	return it->get();
-}
 
-void Node::copy(Node* source, Node* destination) {
-	Node* x = NULL;
-
-	destination = new Node(source->name, source->content);
-	
-	list<unique_ptr<Node>> ::iterator it;
-
-	for (it = source->child.begin(); it != source->child.end(); it++) {
-		it->get()->copy(it->get(), x);
-		destination->addChild(x);
-
-	}
+	return *it->get();
 }
 
 void Node::addAttribute(string name, string value) {
@@ -43,7 +18,7 @@ void Node::addAttribute(string name, string value) {
 	atb->name = name;
 	atb->value = value;
 
-	attributes.push_back(unique_ptr<Attribute> (atb));
+	attributes.push_back(unique_ptr<Attribute>(atb));
 }
 
 bool Node::hasAttribute(string name, string value) {
@@ -61,50 +36,53 @@ bool Node::hasAttribute(string name, string value) {
 	return flag;
 }
 
-Node* Node::findNode(string name, string attribute, string value) {
+Node& Node::findNode(string name, string content) {
 	Node* x = NULL;
 
-	if (this->name == name && hasAttribute(attribute, value)) {
-		return this;
+	if (this->name == name && this->content == content) {
+		return *this;
 	}
 	else {
 		list <unique_ptr<Node>> ::iterator it;
 		for (it = child.begin(); it != child.end(); it++) {
-			x = (*it)->findNode(name, attribute, value);
+			x = &(it)->get()->findNode(name, content);
 			if (x != NULL)
-				return x;
+				return *x;
 		}
 	}
-
-	return NULL;
+	return *x;
 }
 
-bool Node::removeChild(Node* child) {
-	list<unique_ptr<Node>> ::iterator it, it2;
-	bool x = 0;
+Node& Node::findNode(string name, string attribute, string value) {
+	Node* x = NULL;
 
-	if (x == 1)
-		return x;
-	for (it = this->child.begin(); it != this->child.end(); it++) {
-		if (it->get() == child) {
-			it = this->child.erase(it);
-			return x;
-		}
-		x = it->get()->removeChild(child);
+	if (this->name == name && hasAttribute(attribute, value)) {
+		return *this;
 	}
+	else {
+		list <unique_ptr<Node>> ::iterator it;
+		for (it = child.begin(); it != child.end(); it++) {
+			x = &(it)->get()->findNode(name, attribute, value);
+			if (x != NULL)
+				return *x;
+		}
+	}
+	return *x;
 }
 
-bool Node::removeLinkToParrent(Node* child) {
+
+bool Node::removeLinkToParent(const Node& child) {
 	list<unique_ptr<Node>> ::iterator it, it2;
 	bool x = 0;
 
 	for (it = this->child.begin(); it != this->child.end(); it++) {
 		for (it2 = it->get()->child.begin(); it2 != it->get()->child.end(); it2++) {
-			if (it2->get() == child) {
+			if (it2->get() == &child) {
+				it2->release();
 				it->get()->child.erase(it2);
 				return 1;
 			}
-			x = it->get()->removeLinkToParrent(child);
+			x = it->get()->removeLinkToParent(child);
 			if (x == 1)
 				return 1;
 		}
@@ -134,20 +112,41 @@ void Node::deleteNode() {
 	deleteAttributes();
 
 	if (child.size() > 0) {
-		list<unique_ptr<Node>> ::iterator it = child.begin(), a;
+		list<unique_ptr<Node>> ::iterator it = child.begin();
 		for (it = child.begin(); it != child.end(); it++) {
-			(*it)->deleteAttributes();
 			(*it)->deleteNode();
 		}
 	}
+	int x = removeLinkToParent(*this);
 }
 
 void Node::deleteAttributes() {
 	list<unique_ptr<Attribute>> ::iterator it = attributes.begin();
-	
+
 	while (it != attributes.end()) {
 		it = attributes.erase(it);
 	}
+}
+
+void Node::removeAttribute(string name, string value) {
+	list<unique_ptr<Attribute>> ::iterator it = attributes.begin();
+
+	while (it != attributes.end()) {
+		if (it->get()->name == name && it->get()->value == value) {
+			attributes.erase(it);
+			break;
+		}
+		it++;
+	}
+}
+
+void Node::displayNode() {
+	cout << name << " ";
+	if (content != "") {
+		cout << "with the content: " << content << "and";
+	}
+	cout << "with the attributes: ";
+	displayAttributes();
 }
 
 void Node::displayChildren(int spaces) {
@@ -191,7 +190,7 @@ void Node::displayChildren(int spaces) {
 
 
 
-void Node::printChildren(ofstream &g, int spaces) {
+void Node::printChildren(ofstream& g, int spaces) {
 	int i = spaces;
 	list <unique_ptr<Node>> ::iterator it;
 
